7786c6c1e957d9e7707d4bdc9d009000364e670f
Shiniko <j_culilap1@u.pacific.edu>
2019-05-23 16:45:16 -0700

    Added images

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  appTitle: string = 'Jamie Lynn Culilap';
-  
+  appTitle: string='Jamie Lynn Culilap';
+
   constructor() { }
 
   ngOnInit() {
